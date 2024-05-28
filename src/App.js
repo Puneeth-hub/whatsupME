219a67262ab392a779bd0a67c58f57873cf911d7
Puneeth-hub <puneethkumargajjela@gmail.com>
2024-05-28 14:45:48 +0530

    mechat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './logowhats.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href="https://wa.me/+918142424331?text=hello buddy">
+        <button class="whatsapp-button">
+            <img src={logo} alt="WhatsApp Chat"/>
+            Chat
+        </button>
+        <p>👆👆👆👆</p>
+    </a>
+
     </div>
   );
 }
